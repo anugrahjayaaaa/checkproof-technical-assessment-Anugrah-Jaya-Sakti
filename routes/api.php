32753ccfd3ccac850bc483d9ab8bdc1566e0b73e
Anugrah Jaya <30459937+anugrahjayaaaa@users.noreply.github.com>
2026-09-08 18:13:16 +0700

    add api route

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,10 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+
+Route::group(['prefix' => 'users'], function () {
+    Route::get('/', [UserController::class, 'index']);
+    Route::post('/', [UserController::class, 'store']);
+});
