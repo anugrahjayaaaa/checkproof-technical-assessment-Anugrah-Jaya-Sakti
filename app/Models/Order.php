14098cf9cc727b9ef681b51d3dd420ecc7416309
Anugrah Jaya <30459937+anugrahjayaaaa@users.noreply.github.com>
2026-09-08 16:13:34 +0700

    create new model and migration for order

---
+++ app/Models/Order.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Attributes\Fillable;
+use Illuminate\Database\Eloquent\Model;
+
+#[Fillable(['user_id'])]
+class Order extends Model
+{
+    //
+}
