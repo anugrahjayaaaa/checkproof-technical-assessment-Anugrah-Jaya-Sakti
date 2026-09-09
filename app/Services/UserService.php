1be0418684ee750773bf9e9e25eaf5b7fd8e2776
Anugrah Jaya <30459937+anugrahjayaaaa@users.noreply.github.com>
2026-09-09 21:01:11 +0700

    implement try catch when sending email

--- app/Services/UserService.php
+++ app/Services/UserService.php
@@ -5,6 +5,7 @@ namespace App\Services;
 use App\Mail\AdminNewUserNotificationMail;
 use App\Mail\UserCreateMail;
 use App\Models\User;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Mail;
 
 class UserService
@@ -13,14 +14,20 @@ class UserService
     {
         $user = User::create($data);
 
-        //send email to user
-        Mail::to($user->email)
-            ->send(new UserCreateMail($user, $data['password']));
-        
-        //send mail to admin
-        Mail::to(config('mail.admin_email'))
-            ->send(new AdminNewUserNotificationMail($user));
+        try {
+            // Send email to user
+            Mail::to($user->email)
+                ->send(new UserCreateMail($user, $data['password']));
 
+            // Send mail to admin
+            Mail::to(config('mail.admin_email'))
+                ->send(new AdminNewUserNotificationMail($user));
+        } catch (\Throwable $e) {
+            Log::error('Failed to send user creation email: ' . $e->getMessage(), [
+                'user_id' => $user->id,
+                'exception' => $e,
+            ]);
+        }
         return $user;
     }
 }
